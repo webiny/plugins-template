ba254499a885061d4c7c6a72aece2c64af0f9a3c
Pavel Denisjuk <pavel@webiny.com>
2022-01-12 09:56:25 +0100

    chore: repo cleanup

--- packages/button/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Button from "./Button";
-
-/**
- * Export the Button React component so it is accessible via the @custom-components/button
- */
-export { Button };
--- packages/button/webiny.config.ts
+++
@@ -1,8 +0,0 @@
-import { createBuildPackage, createWatchPackage } from "@webiny/project-utils";
-
-export default {
-    commands: {
-        build: createBuildPackage({ cwd: __dirname }),
-        watch: createWatchPackage({ cwd: __dirname })
-    }
-};
